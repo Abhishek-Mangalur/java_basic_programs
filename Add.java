4b4d4ce03be47895b7cb4bced3f00a4d1d8435db
Abhishek <abhishekm.21.beis@acharya.ac.in>
2023-01-01 16:09:23 +0530

    Addition of two no.

---
+++ Add.java
@@ -0,0 +1,11 @@
+public class Add
+{
+    public static void main(String[] args) 
+    {
+        int a,b,sum;
+        a = 10;
+        b = 20;
+        sum = a + b;
+        System.out.println("The Sum is = "+sum);
+    }
+}
